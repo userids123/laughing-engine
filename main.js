0b9c1c8b4dcf2440e5966defae58eb6423fd6165
userids123 <106725107+userids123@users.noreply.github.com>
2022-07-30 15:15:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+Status="";
+objects=[];
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.position(40,250);
+    video = createCapture(VIDEO);
+    video.size(300,290);
+    video.hide();
+}
+window.addEventListener("keydown", my_keydown);
+ function my_keydown(e){
+     keyPressed = e.keyCode;
+     console.log(keyPressed);
+     if(keyPressed=='13'){
+         start();
+         console.log("key is pressed.");
+     }
+    }
+
+function start(){
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Object";
+    input_text = document.getElementById("input_box").value;
+}
+function modelLoaded(){
+    console.log("Model_Loaded");
+    Status = true;
+}
+function draw(){
+    image(video,0,0,600,500);
+    if(Status != ""){
+        object_Detector.detect(video, gotResults);
+        for(i = 0;i < objects.length;i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            console.log(objects.length);
+            fill("#ff0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x + 15,objects[i].y + 15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label == input_text){
+                video.stop();
+                object_Detector.detect(gotResults);
+                document.getElementById("objf").innerHTML = input_text+" Found";
+                var synth = window.speechSynthesis;
+                var utterThis = new SpeechSynthesisUtterance(input_text + "Found");
+                synth.speak(utterThis);
+            }
+            else{
+                document.getElementById("objf").innerHTML = input_text + " Not Found";
+            }
+        }
+    }
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
